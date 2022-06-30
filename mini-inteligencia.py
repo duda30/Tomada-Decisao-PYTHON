ed42f639b6e8d2b86c3d82df549db4d60676da61
duda30 <dudalombardi30@gmail.com>
2022-06-30 00:24:24 -0300

    Criação de variáveis

---
+++ mini-inteligencia.py
@@ -0,0 +1,17 @@
+primeironumero=int(input("Digite o primeiro numero "))
+segundonumero=int(input("Digite o segundo numero "))
+terceironumero=int(input("Digite o terceiro numero "))
+
+if primeironumero == segundonumero and terceironumero == primeironumero:
+    print("O menor número é ", primeironumero)
+    
+else:
+    if primeironumero < segundonumero and primeironumero < terceironumero:
+        print("O menor numero é: ", primeironumero)
+        
+    else:
+        if segundonumero < primeironumero and segundonumero < terceironumero:
+            print("O menor numero é: ", segundonumero)
+            
+        else:
+            print("O menor numero é: ", terceironumero)
